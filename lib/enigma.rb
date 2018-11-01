8148ade3135d1ef57c2c1d73ea908231753cc788
Leah Miller <lkmiller.87@gmail.com>
2018-11-01 15:23:02 -0600

    Add test for last four digits

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,4 +2,11 @@ require 'date'
 
 class Enigma
 
-end 
+ def encrypt(message, key = "12345", date = Date.today)
+
+ end
+
+ def offset_by_date(date)
+   date.strftime('%d%m%y').to_i ** 2
+ end
+end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -4,8 +4,29 @@ require_relative './test_helper'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
+  def setup
+    @e = Enigma.new
+  end
   def test_it_exists
-    e = Enigma.new
-    assert_instance_of Enigma, e
+    assert_instance_of Enigma, @e
+  end
+
+  # def test_it_can_encrypt_my_message
+  #   my_message = "this is so secret ..end.."
+  #   output_1 = @e.encrypt(my_message, "12345", Date.today)
+  #   assert_equal '12345ljk', output_1
+  #   output_2 = @e.encrypt(my_message)
+  #   assert_equal '1234lkjuoiu', output_2
+  # end
+
+  def test_it_can_offset_by_date
+    date = Date.parse("August 4, 1995")
+    assert_equal 1672401025, @e.offset_by_date(date)
+  end
+
+  def test_it_can_select_last_four_digits_of_offset
+    date = Date.parse("August 4, 1995")
+    @e.offset_by_date(date)
+    assert_equal 1025, @se.last_four_digits
   end
 end
