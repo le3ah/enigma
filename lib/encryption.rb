12f3af60df212df69a3bfe49661005f9ec7f75f4
Leah Miller <lkmiller.87@gmail.com>
2018-11-03 16:38:19 -0600

    Add new test for encryption class

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -4,7 +4,7 @@ class Encryption
     @character_set = ("a".."z").to_a << " "
   end
 
-  def rotate(letter, shift_value)
-    letter.tr(@character_set.to_s, @character_set.rotate(shift_value).to_s)
+  def rotate(message, shift_value)
+    message.tr(@character_set.to_s, @character_set.rotate(shift_value).to_s)
   end
 end
--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -14,8 +14,14 @@ class EncryptionTest < Minitest::Test
     assert_equal expected, @encryption.character_set
   end
 
-  def test_it_can_rotate_one_letter
-    assert_equal "d", @encryption.rotate("a", 3)
+  def test_it_can_rotate_given_letters_in_message
+    assert_equal "khoor", @encryption.rotate("hello", 3)
+    assert_equal "arx", @encryption.rotate("you", 3)
+  end
+
+  def test_it_can_rotate_based_on_different_shifts
+    shift_value = {A: 3, B: 27, C: 73, D: 20}
+    assert_equal "keder", @encryption.rotate("hello", shift_value)
   end
 
   # def test_it_can_rotate_alphabet_by_final_shift
