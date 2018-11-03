bce482d0ead38b3308a8b2018e73812393bc83f6
Leah Miller <lkmiller.87@gmail.com>
2018-11-03 16:50:02 -0600

    Add changes for working simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
+require 'simplecov'
+SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/enigma'
 require './lib/offset'
 require './lib/encryption'
 require './lib/decryption'
-require 'simplecov'
 require 'date'
-SimpleCov.start
