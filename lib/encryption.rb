63baddf4fd0bc2ff257727680da6b51883a36fd6
Leah Miller <lkmiller.87@gmail.com>
2018-11-03 16:27:18 -0600

    Add passing test for rotate

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -3,4 +3,8 @@ class Encryption
   def initialize
     @character_set = ("a".."z").to_a << " "
   end
+
+  def rotate(letter, shift_value)
+    letter.tr(@character_set.to_s, @character_set.rotate(shift_value).to_s)
+  end
 end
