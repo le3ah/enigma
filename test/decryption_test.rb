65fb24aadff7a0eb548010341dd0da84a45c97cd
Leah Miller <lkmiller.87@gmail.com>
2018-11-03 18:26:36 -0600

    Add passing test for character set initialize value in decryption

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -8,4 +8,9 @@ class DecryptionTest < Minitest::Test
   def test_it_exists
     assert_instance_of Decryption, @decryption
   end
+
+  def test_it_has_a_starting_character_set
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, @decryption.character_set
+  end
 end
