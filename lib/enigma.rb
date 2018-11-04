896c5752ba79fdd1a07a701b4b78ddc2a2f608d8
Leah Miller <lkmiller.87@gmail.com>
2018-11-04 09:10:04 -0700

    Add passing test for encrypt method in enigma controller

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,11 +8,18 @@ class Enigma
     @date = date
   end
 
-  # def encrypt(message, key = "12345", date = Date.today)
-  #
-  #  @key = key
-  #  @date = date
-  # end
+  def encrypt(message, key, date)
+    @date = date
+    @key = key
+    offset = Offset.new(date, key)
+    encryption = Encryption.new
+    output_message = encryption.rotate(message, offset.final_shift)
+    output = {}
+    output[:encryption] = output_message
+    output[:key] = key
+    output[:date] = date
+    output
+  end
 
 
 
